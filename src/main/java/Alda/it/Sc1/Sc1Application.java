2e6e6f4973c15bba9740ebc0aa8cb338833b7aa5
Alda Drinalda H <drinalda@yahoo.com>
2025-06-27 13:41:34 +0200

    first commit

---
+++ src/main/java/Alda/it/Sc1/Sc1Application.java
@@ -0,0 +1,13 @@
+package Alda.it.Sc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/Alda/it/Sc1/Sc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Alda.it.Sc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
